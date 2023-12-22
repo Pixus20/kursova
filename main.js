0dcf0513854f54b6eb11ccfe4fc9f8e4a665fa32
Pixus20 <152525734+Pixus20@users.noreply.github.com>
2023-12-22 21:05:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+/* слайдер*/ 
+const images = document.querySelectorAll('.slider-img');
+const controlls = document.querySelectorAll('.controlls');
+let imageIndex = 0;
+
+function show(index) {
+   images[imageIndex].classList.remove('active');
+   images[index].classList.add('active');
+   imageIndex = index;
+}
+
+controlls.forEach((e) => {
+   e.addEventListener('click', () => {
+      if (e.classList.contains('prev')) {
+         let index = imageIndex - 1;
+
+         if (index < 0) {
+            index = images.length - 1;
+         }
+         show(index);
+      } else if (e.classList.contains('next')) {
+         let index = imageIndex + 1;
+
+         if (index >= images.length) {
+            index = 0;
+         }
+         show(index);
+      }
+   })
+})
+show(imageIndex);
+/*меню юзера*/
